2a7082ce2464c396ff06af42cf2404a0d4f36fcc
hlaaftana <atadorukaydin@gmail.com>
2016-01-02 09:59:18 +0200

    I'm tired, I'll patch these later

--- src/hlaaftana/discordg/objects/Channel.groovy
+++ src/hlaaftana/discordg/objects/Channel.groovy
@@ -11,6 +11,7 @@ class Channel extends Base{
 	boolean isPrivate(){ return object.getBoolean("is_private") }
 	String getPosition(){ return object.getInt("position") }
 	String getType(){ return object.getString("type") }
+	String getTopic(){ return object.getString("topic") }
 	Server getServer(){ if (this.isPrivate()) return null
 		for (s in api.client.getServers()){
 			if (s.getID().equals(object.getString("guild_id"))) return s
@@ -22,6 +23,6 @@ class Channel extends Base{
 	}
 
 	Channel edit(Map<String, Object> data) {
-		return null
+		return new Channel(api, api.getRequester().patch("https://discordapp.com/api/channels/${this.getID()}", new JSONObject().put("name", (data.containsKey("name")) ? data["name"].toString() : this.getName()).put("position", (data.containsKey("position")) ? data["position"] : this.getPosition()).put("topic", (data.containsKey("topic")) ? data["topic"].toString() : this.getTopic())))
 	}
 }
--- src/hlaaftana/discordg/objects/Member.groovy
+++ src/hlaaftana/discordg/objects/Member.groovy
@@ -3,6 +3,7 @@ package hlaaftana.discordg.objects
 import java.util.List
 
 import java.net.URL
+import org.json.JSONArray
 import org.json.JSONObject
 
 class Member extends User{
@@ -10,16 +11,29 @@ class Member extends User{
 		super(api, object)
 	}
 
-	Member getMember(Server server) {
-		return null
+	User getUser() {
+		return new User(api, object.getJSONObject("user"))
 	}
 
 	Server getServer() {
-		return null
+		for (s in api.client.getServers()){
+			if (s.getID().equals(object.getString("guild_id"))) return s
+		}
 	}
 
 	List<Role> getRoles() {
-		return null
+		JSONArray array = object.getJSONArray("roles")
+		List<Role> roles = new ArrayList<Role>()
+		for (int i = 0; i < Short.MAX_VALUE; i++){
+			try{
+				for (r in this.getServer().getRoles()){
+					if (r.getID().equals(array.get(i))) roles.add(r)
+				}
+			}catch (e){
+				break
+			}
+		}
+		return roles
 	}
 
 	void editRoles(List<Role> roles) {
