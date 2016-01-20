149e54a1c5d32c09831760da09c92c397e914d75
hlaaftana <atadorukaydin@gmail.com>
2016-01-20 03:54:42 +0200

    Added moving a member to voice channels to member of voice move member

--- src/ml/hlaaftana/discordg/objects/VoiceChannel.groovy
+++ src/ml/hlaaftana/discordg/objects/VoiceChannel.groovy
@@ -6,4 +6,8 @@ package ml.hlaaftana.discordg.objects
  */
 class VoiceChannel extends Channel{
 	VoiceChannel(API api, Map object){ super(api, object) }
+
+	void moveMember(Member member){
+		api.requester.patch("https://discordapp.com/api/guilds/${member.server.id}/members/{member.id}", ["channel_id": this.id])
+	}
 }
