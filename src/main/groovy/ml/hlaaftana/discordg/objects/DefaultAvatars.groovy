2ec8c4fa00446ad00539527d00ed0862333f314b
hlaaftana <atadorukaydin@gmail.com>
2016-03-11 22:07:11 +0200

    Gradle ily but please

--- src/main/groovy/ml/hlaaftana/discordg/objects/DefaultAvatars.groovy
+++ src/main/groovy/ml/hlaaftana/discordg/objects/DefaultAvatars.groovy
@@ -6,7 +6,7 @@ enum DefaultAvatars {
 	GRAY("0d1a93187d96a05e86444f2fc6210d95", 1),
 	GREEN("a83f572c0b5c2d87f935ce6229be6358", 2),
 	YELLOW("907c319873ae4c1d56d0d0e8dce6b476", 3),
-	RED("8b3fac6205178732d218265987cdb0dc", 4),
+	RED("8b3fac6205178732d218265987cdb0dc", 4)
 
 	String hash
 	int order
